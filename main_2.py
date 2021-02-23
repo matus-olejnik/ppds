8f58233e6897b69865c817ddc4ad35a39ed40c1f
Matus Olejnik <matus.olejnik@gmail.com>
2021-02-23 02:58:01 +0100

    feat: second demonstration of non working solution with deadlock
    Táto implementácia predstavuje demonštráciu nefunkčnej synchronizácie. Nakoľko jednotlivé vlákna sú počas behu synchronizované tak pri poslednom kroku keď jedno z nich vykoná úspešnú podmienku, že counter je väčší alebo rovný ako veľkosť poľa tak príde k ukončeniu while cyklu bez toho, že by vlákno uvoľnilo zámok a teda ostatné vlákna budú čakať donekonečna. Nastáva tu deadlock.
    
    Refs #1

---
+++ main_2.py
@@ -0,0 +1,43 @@
+from fei.ppds import Thread, Mutex
+
+
+class Shared():
+    def __init__(self, end):
+        self.counter = 0
+        self.end = end
+        self.array = [0] * self.end
+        self.mutex = Mutex()
+
+
+def print_histogram(array):
+    # Printing histogram of values from 0 (left) to 9 (right).
+
+    hist = [0] * 10
+    for i in array:
+        hist[i] += 1
+
+    print(hist)
+
+
+def counter(shared):
+    while True:
+        shared.mutex.lock()
+
+        if shared.counter >= shared.end:
+            break
+
+        shared.array[shared.counter] += 1
+        shared.counter += 1
+
+        shared.mutex.unlock()
+
+
+for _ in range(10):
+    sh = Shared(1_000_000)
+    t1 = Thread(counter, sh)
+    t2 = Thread(counter, sh)
+
+    t1.join()
+    t2.join()
+
+    print_histogram(sh.array)
